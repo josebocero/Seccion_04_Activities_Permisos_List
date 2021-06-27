6f364c367e6f87891e9bf420ac8301df4c0982bf
josebocero <josebocero@gmail.com>
2021-06-28 00:17:36 +0200

    video 38 boton back + Handler

--- app/src/main/java/jose_g_bocero/com/jose/seccion_04_activities_permisos_list/activities/LifeCycleActivity.kt
+++ app/src/main/java/jose_g_bocero/com/jose/seccion_04_activities_permisos_list/activities/LifeCycleActivity.kt
@@ -2,11 +2,27 @@ package jose_g_bocero.com.jose.seccion_04_activities_permisos_list.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import jose_g_bocero.com.jose.seccion_04_activities_permisos_list.R
 
 class LifeCycleActivity : LifeCycleEventsActivity() {
+
+    private var exitEnabled = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_cycle)
     }
+
+    override fun onBackPressed() {
+        if (exitEnabled) {
+            super.onBackPressed()
+        }
+        exitEnabled = true
+        Toast.makeText(this, "Click back again to exit this sreen", Toast.LENGTH_LONG).show()
+        Handler(Looper.getMainLooper()).postDelayed(Runnable { exitEnabled = false }, 2000)
+    }
+
 }
